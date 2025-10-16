c9bad0c3801348527b0c8647cc8a8c25e2da1653
abhilekhx <lekh7adhikari@gmail.com>
2025-10-15 23:10:03 -0400

    added navbar component with contact and social icons

--- app/page.js
+++ app/page.js
@@ -1,23 +1,9 @@
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import AddReactionIcon from "@mui/icons-material/AddReaction";
-import AddCallIcon from "@mui/icons-material/AddCall";
+import TopNav from "@/components/topnav/TopNav";
 
 export default function Home() {
   return (
     <div>
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-      </Stack>
-      <Button color="secondary">Secondary</Button>
-      <Button variant="contained" color="success">
-        <AddCallIcon></AddCallIcon>Success
-      </Button>
-      <Button variant="outlined" color="error">
-        Error<AddReactionIcon></AddReactionIcon>
-      </Button>
+      <TopNav />
     </div>
   );
 }
